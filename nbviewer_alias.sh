2ed4731612a544836315a14d745e6328e454c243
Arielle Blum <73490402+amblum@users.noreply.github.com>
2021-02-25 08:41:13 -0700

    Create nbviewer_alias.sh

---
+++ nbviewer_alias.sh
@@ -0,0 +1,2 @@
+nbviewer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho nbviewer /tmp/test.ipynb\ncat /tmp/test.ipynb | nbviewer test.ipynb"; return 1; fi 
+tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; (cat $tmpfile | sed s@https://@https://nbviewer.jupyter.org/urls/@g); rm -f $tmpfile; echo ""; } 
